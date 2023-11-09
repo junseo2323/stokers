1feaade1c8a0b8a25aafb43b0bc75d034644aaf2
junseo23 <hunseol03@gmail.com>
2023-11-10 03:31:20 +0900

    햄버거 수정

--- frontend/myapp/src/components/Burgercomp.js
+++ frontend/myapp/src/components/Burgercomp.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import "./Burgercomp.scss";
 
 const Burgercomp = () => {
 
     return (
         <div className='Buger'>
+            <button className='ExitButton'>X</button>
             <ul>
-                <li>test</li>
-                <li>test</li>
-                <li>test</li>
+                <li>
+                    <Link to="/">개인정보 관리</Link>
+                </li>
+                <li>
+                    <Link to="/">퀘스트</Link>
+                </li>
+                <li>
+                    <Link to="/">퀘스트 랭크</Link>
+                </li>
+                <li>
+                    <Link to="/">단기 / 중장기 추천</Link>
+                </li>
+                <li>
+                    <Link to="/">최근 주식 이슈 요약</Link>
+                </li>
+                <li>
+                    <Link to="/" className='logout'>로그아웃</Link>
+                </li>
             </ul>
         </div>
     )
