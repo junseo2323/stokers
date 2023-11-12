8fc30b87c9839b148acf7758d02015506deed35c
junseo23 <hunseol03@gmail.com>
2023-11-12 21:04:04 +0900

    햄버거 최종 완성

--- frontend/myapp/src/components/Nav.js
+++ frontend/myapp/src/components/Nav.js
@@ -5,8 +5,8 @@ import Burgercomp from "./Burgercomp";
 import { Outlet } from "react-router"
 import { AuthContext } from "../utils/user-Api";
 import { Link } from 'react-router-dom';
-import "./Burgercomp.scss";
 
+import "./Burgercomp.scss";
 import './Nav.scss';
 
 const Nav = () => {
@@ -18,7 +18,7 @@ const Nav = () => {
         document.querySelector('.Buger').setAttribute("id", "OnBuger");
         document.querySelector('.element_container').setAttribute("id", "OffBugerElement");
         
-    }
+    } 
 
     const onBugerClick = () => {
         setOnbugger(!onbugger);
@@ -31,27 +31,27 @@ const Nav = () => {
         <>
             <div className='Nav_componenet' id='Nav_componenet'>
                 <div className='Buger' id='OffBuger'>
-                <button className='ExitButton' onClick={onBugerClick}>X</button>
-                <ul>
-                    <li>
-                        <Link to="/">개인정보 관리</Link>
-                    </li>
-                    <li>
-                        <Link to="/">퀘스트</Link>
-                    </li>
-                    <li>
-                        <Link to="/">퀘스트 랭크</Link>
-                    </li>
-                    <li>
-                        <Link to="/">단기 / 중장기 추천</Link>
-                    </li>
-                    <li>
-                        <Link to="/">최근 주식 이슈 요약</Link>
-                    </li>
-                    <li>
-                        <Link to="/" onClick={logoutUser} className='logout'>로그아웃</Link>
-                    </li>
-                </ul>
+                    <button className='ExitButton' onClick={onBugerClick}>X</button>
+                    <ul>
+                        <li>
+                            <Link to="/">개인정보 관리</Link>
+                        </li>
+                        <li>
+                            <Link to="/">퀘스트</Link>
+                        </li>
+                        <li>
+                            <Link to="/">퀘스트 랭크</Link>
+                        </li>
+                        <li>
+                            <Link to="/">단기 / 중장기 추천</Link>
+                        </li>
+                        <li>
+                            <Link to="/">최근 주식 이슈 요약</Link>
+                        </li>
+                        <li>
+                            <Link to="/" onClick={logoutUser} className='logout'>로그아웃</Link>
+                        </li>
+                    </ul>
                 </div>
                 <div className='element_container' id='OnBugerElement'>
                     <img src={logo} className='logo' />
