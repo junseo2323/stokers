7677da6b4229760acc470054d1b32b2de0c87e48
junseo23 <hunseol03@gmail.com>
2023-11-15 23:41:07 +0900

    231115

--- frontend/myapp/src/components/Text.js
+++ frontend/myapp/src/components/Text.js
@@ -4,24 +4,44 @@ import store from "../store/store";
 import { fetchQUESTDETAIL } from "../actions/fetchQuestdetail"
 import { useSelector } from "react-redux";
 import { useParams, useNavigate } from 'react-router-dom';
+import Swal from "sweetalert2";
 import './Text.scss';
 
 const Text = () =>
 {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'center-center',
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
+
   let { id } = useParams();
   const {qstatus,updatestatusUser,submitTextmission,user}  = useContext(AuthContext);
   const navigator = useNavigate();
   const [textfile,setTextfile] = useState("");
 
-  useEffect(()=>{
+  useEffect(()=>{ 
     store.dispatch(fetchQUESTDETAIL(id));
   } ,[]) 
 
   const handleSubmit = async e => {
     e.preventDefault();
+    Toast.fire({
+      icon: 'success',
+      title: '전송 성공!'
+    });
+    updatestatusUser(user.username,qstatus[user.user_id-1].status+1);
     submitTextmission(id,user.user_id,textfile);
-    navigator("/quest");
-  };
+    setTimeout(function(){
+      window.location.replace("/quest");
+  }, 1500); //1초 뒤에 메인 퀘스트 페이지로 전황
+};
 
   const detailstate = useSelector(state => state.questdetail.QUESTDETAIL);
  
